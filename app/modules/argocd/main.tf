3d88c83b88f42dae8d5397f4a871e400453a9444
chanchalsonitw <chanchal.soni@thoughtworks.com>
2023-12-06 11:39:51 +0530

    CS | NP | MJ add argocd using helm

---
+++ app/modules/argocd/main.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "argocd" {
+  name       = "argocd-demoproject"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace = "argocd"
+  create_namespace = true
+#  values = [
+#    file("./values.yaml")
+#  ]
+}
---
+++ app/stacks/argocd/variable.tf
@@ -0,0 +1,10 @@
+variable "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+}
+
+variable "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+}
+variable "cluster_id" {
+  description = "ID or name of the EKS cluster"
+}
