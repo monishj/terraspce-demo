2e7b9916205f638f5a893765a5f3554da959c9c7
Pragati007 <pragati.singh@thoughtworks.com>
2023-12-13 11:19:08 +0530

    Update. talismanrc

--- app/stacks/argocd/argocd-repositories.tf
+++ app/stacks/argocd/argocd-repositories.tf
@@ -1,20 +1,20 @@
-locals {
-  code_commit_secret = <%= aws_secret("argocd-codecommit-connect") %>
-}
-
-resource "kubernetes_secret" "argocd-repo-secret" {
-  metadata {
-    name = "codecommit-argocd-repo-secret"
-    namespace = "argocd"
-    labels = {
-      "argocd.argoproj.io/secret-type": "repository"
-    }
-  }
-  data = {
-    username = local.code_commit_secret["username"]
-    password = local.code_commit_secret["password"]
-    url = "https://git-codecommit.me-central-1.amazonaws.com/v1/repos/project-demo-repo"
-  }
-  depends_on = [module.argocd]
-}
-
+#locals {
+#  code_commit_secret = <%= aws_secret("argocd-codecommit-connect") %>
+#}
+#
+#resource "kubernetes_secret" "argocd-repo-secret" {
+#  metadata {
+#    name = "codecommit-argocd-repo-secret"
+#    namespace = "argocd"
+#    labels = {
+#      "argocd.argoproj.io/secret-type": "repository"
+#    }
+#  }
+#  data = {
+#    username = local.code_commit_secret["username"]
+#    password = local.code_commit_secret["password"]
+#    url = "https://git-codecommit.me-central-1.amazonaws.com/v1/repos/project-demo-repo"
+#  }
+#  depends_on = [module.argocd]
+#}
+#
