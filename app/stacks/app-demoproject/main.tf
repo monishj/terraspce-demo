6625d9b2ab2fd6aed5cdc6d71931b6c3b784463c
Monish Jain <monish.jain@thoughtworks.com>
2023-12-13 18:28:56 +0530

    [Pipelines] Fixes

--- app/stacks/app-demoproject/main.tf
+++ app/stacks/app-demoproject/main.tf
@@ -12,24 +12,16 @@ resource "kubernetes_manifest" "application_argocd_adcb_argo" {
         "server" = "https://kubernetes.default.svc"
       }
       "project" = "default"
-      "sources" = [
-        {
-          "ref" = "values"
-          "repoURL" = "https://github.com/chanchalsonitw/project-demo-app.git"
-          "targetRevision" = "master"
-        },
-        {
-          "chart" = "projectdemo-helm-chart"
-          "helm" = {
-            "releaseName" = "projectdemo-release"
-            "valueFiles" = [
-              "$values/demo-project-deploy/projectdemo-helm-chart/values.yaml",
-            ]
-          }
-          "repoURL" = "https://github.com/chanchalsonitw/project-demo-app/demo-project-deploy/projectdemo-helm-chart"
-          "targetRevision" = "0.1.0"
-        },
-      ]
+      "source" = {
+        "helm" = {
+          "valueFiles" = [
+            "values.yaml",
+          ]
+        }
+        "path"           = "demo-project-deploy/projectdemo-helm-chart"
+        "repoURL"        = "https://github.com/chanchalsonitw/project-demo-app.git"
+        "targetRevision" = "master"
+      }
       "syncPolicy" = {
         "automated" = {
           "prune" = true
