a1427b3cb32df39a8f40da050dbe55f75f4c216f
Chanchal <chanchal.soni@thoughtworks.com>
2023-12-13 12:38:27 +0000

    [argocd-app][infra-as-code] comment helm repo secret for argocd

--- app/stacks/argocd/argocd-helm-repositories.tf
+++ app/stacks/argocd/argocd-helm-repositories.tf
@@ -1,23 +1,23 @@
-locals {
-  helm_repo_secret = <%= aws_secret("argocd-helm-repo-connect") %>
-}
+#locals {
+#  helm_repo_secret = <%= aws_secret("argocd-helm-repo-connect") %>
+#}
 
-resource "kubernetes_secret" "argocd-helm-repo-secret" {
-  metadata {
-    name = "helm-argocd-repo-secret"
-    namespace = "argocd"
-    labels = {
-      "argocd.argoproj.io/secret-type": "repository"
-    }
-  }
-  data = {
-    username = local.helm_repo_secret["username"]
-    password = local.helm_repo_secret["password"]
-    type = "helm"
-    name = "projectdemo-helm-chart"
-    url = "908049645973.dkr.ecr.me-central-1.amazonaws.com"
-    enableOCI = "true"
-    project = "default"
-  }
-  depends_on = [module.argocd]
-}
+#resource "kubernetes_secret" "argocd-helm-repo-secret" {
+#  metadata {
+#    name = "helm-argocd-repo-secret"
+#    namespace = "argocd"
+#    labels = {
+#    "argocd.argoproj.io/secret-type": "repository"
+#  }
+#}
+#  data = {
+#  username = local.helm_repo_secret["username"]
+#  password = local.helm_repo_secret["password"]
+#  type = "helm"
+#  name = "projectdemo-helm-chart"
+#  url = "908049645973.dkr.ecr.me-central-1.amazonaws.com"
+#  enableOCI = "true"
+#  project = "default"
+#}
+#  depends_on = [module.argocd]
+#}
